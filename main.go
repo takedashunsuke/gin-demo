20a1c7633efc3c1b0380ea479d647ca03d5c8e2b
bien-takeda-at-207198706198 <shunsuke.takeda@exhiwork.co.jp>
2025-07-06 10:35:23 +0900

    / 200

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080" // ローカル開発用
+    }
+
     r := gin.Default()
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
             "message": "pong",
         })
     })
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "Hello, Gin Home!"})
+	})
+
     r.Run() // デフォルトで :8080 で起動
 }
